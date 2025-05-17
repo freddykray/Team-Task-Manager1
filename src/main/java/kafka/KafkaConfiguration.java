9ed7dd87886fee87450c6d2997180f42b01d1738
freddykray <rosi.kanal2017@yandex.ru>
2025-05-17 19:20:34 +0300

    Добавлен метод newTopic

--- src/main/java/kafka/KafkaConfiguration.java
+++ src/main/java/kafka/KafkaConfiguration.java
@@ -1,7 +1,18 @@
 package kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaConfiguration {
+
+    @Bean
+    public NewTopic newTopic(){
+        return new NewTopic(
+                "notification",
+                1,
+                (short) 1
+        );
+    }
 }
