93c4e876677203b61d4d210bedb226272a21a1df
freddykray <rosi.kanal2017@yandex.ru>
2025-05-17 19:26:46 +0300

    Добавлен KafkaTemplate и конструктор

--- src/main/java/kafka/KafkaProducer.java
+++ src/main/java/kafka/KafkaProducer.java
@@ -1,7 +1,15 @@
 package kafka;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class KafkaProducer {
+    @Autowired
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+
 }
